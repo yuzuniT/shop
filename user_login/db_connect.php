2c0547d63595f3a6fcb5c540c18e1cfa63b1e362
yuzuniT <inukoro@live.jp>
2025-05-22 11:54:52 +0900

    Composerを使用してDotenvを導入した。 元々db_connectに接続情報を書き込んでいたのをconfig内の.envファイルに切り離した。

--- user_login/db_connect.php
+++ user_login/db_connect.php
@@ -1,17 +1,25 @@
 <?php
-/* ①　データベースの接続情報を定数に格納する */
-const DB_HOST = 'mysql:dbname=user_login;host=localhost';
-const DB_USER = 'root';
-const DB_PASSWORD = 'u12TrEXQ';
+/* Dotenvを利用し、config/db_info.envから接続に必要な情報を持ってくる */
+require __DIR__.'/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../config');
+$dotenv->load();
+
+const DB_dsn = 'mysql:dbname=' . $_ENV['DB_NAME'] . ';host=' . $_ENV['DB_HOST'] . ';charset=utf8mb4';
+const DB_USER = $_ENV['DB_USER'];
+const DB_PASSWORD = $_ENV['DB_PASSWORD'];
 
 //②　例外処理を使って、DBにPDO接続する
 try {
-    $pdo = new PDO(DB_HOST,DB_USER,DB_PASSWORD,[
+    $pdo = new PDO(DB_dsn,DB_USER,DB_PASSWORD,[
 		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		PDO::ATTR_EMULATE_PREPARES =>false
 	]);
 } catch (PDOException $e) {
-	echo 'ERROR: Could not connect.'.$e->getMessage()."\n";
+	echo 'エラー：データベースに接続できませんでした'.$e->getMessage()."\n";
 	exit();
 }
+
+
+?>
