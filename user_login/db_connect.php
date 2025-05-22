0a68dd63d4376e35f1475e96946f007510b4a12e
yuzuniT <inukoro@live.jp>
2025-05-22 22:05:32 +0900

    fix : 定数宣言をconstからdefine関数利用に変更

--- user_login/db_connect.php
+++ user_login/db_connect.php
@@ -5,13 +5,14 @@ require __DIR__.'/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../config');
 $dotenv->load();
 
-const DB_dsn = 'mysql:dbname=' . $_ENV['DB_NAME'] . ';host=' . $_ENV['DB_HOST'] . ';charset=utf8mb4';
-const DB_USER = $_ENV['DB_USER'];
-const DB_PASSWORD = $_ENV['DB_PASSWORD'];
+/* 環境変数を含むためconstでなくdefine関数で定数宣言 */ 
+define('DB_DSN', 'mysql:dbname=' . $_ENV['DB_NAME'] . ';host=' . $_ENV['DB_HOST'] . ';charset=utf8mb4');
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
 
 //②　例外処理を使って、DBにPDO接続する
 try {
-    $pdo = new PDO(DB_dsn,DB_USER,DB_PASSWORD,[
+    $pdo = new PDO(DB_DSN,DB_USER,DB_PASSWORD,[
 		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		PDO::ATTR_EMULATE_PREPARES =>false
